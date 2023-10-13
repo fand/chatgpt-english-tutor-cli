2b872b8ba293ff16c0b1556083c1bf5ad6ae27e2
fand <mail@amagi.dev>
2023-10-13 11:51:46 -0700

    init

---
+++ README.md
@@ -0,0 +1,63 @@
+# ChatGPT English Tutor CLI
+
+![Example of the commands](./example.png)
+
+This repository contains multiple commands to assist with learning and writing English.
+
+
+## Setup
+
+First, install the commands using npm:
+```
+npm i -g @fand/chatgpt-english-tutor
+```
+
+Then save your OpenAI API key to `~/.config/chatgptenglish` like this:
+
+```
+OPENAI_API_KEY=XXXXXXXXXXXXXXXXXXXX
+```
+
+## Usage
+
+### `naturalize`
+
+This command transforms the given text into more natural-sounding English.
+
+Usage:
+```
+$ naturalize THE SENTENCE YOU WANT TO NATURALIZE
+```
+
+Example:
+```
+$ naturalize "All your base are belong to us"
+All your bases are now under our control.
+
+$ naturalize this command makes the given text into more natural English.
+This command transforms the given text into more natural-sounding English.
+```
+
+### `rephrase`
+
+This command provides multiple rephrasings of the given text.
+
+Example:
+```
+$ rephrase
+
+1. Become a citizen: Grant citizenship status.
+2. Adapt to environment: Adjust to a new environment.
+3. Normalize: Make typical or usual.
+4. Accustom: Become accustomed to something.
+5. Familiarize: Make familiar or acquainted.
+```
+
+## Author
+
+AMAGI ([GitHub](https://github.com/fand), [Threads](https://www.threads.net/@amagitakayosi))
+
+
+## LICENSE
+
+MIT
---
+++ bin/naturalize.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+import { Configuration, OpenAIApi } from "openai";
+import rc from "rc";
+
+const OPENAI_API_KEY = rc("chatgptenglish").OPENAI_API_KEY;
+if (!OPENAI_API_KEY) {
+  console.error("Please save your API key in `~/.config/chatgptenglish` file.");
+  process.exit(1);
+}
+
+const configuration = new Configuration({
+  apiKey: OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+(async function main() {
+  if (!configuration.apiKey) {
+    process.exit(1);
+  }
+
+  const input = process.argv.slice(2).join(" ");
+  await run(input);
+})();
+
+async function run(text) {
+  try {
+    const prompt = generatePrompt(text);
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt,
+      temperature: 0.6,
+      max_tokens: 2000,
+    });
+    const result = completion.data.choices[0].text;
+    console.log(result);
+  } catch (error) {
+    // Consider adjusting the error handling logic for your use case
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+    }
+  }
+}
+
+function generatePrompt(text) {
+  return `Please edit the text below to make it sound more natural.
+
+====
+${text}
+====
+`;
+}
