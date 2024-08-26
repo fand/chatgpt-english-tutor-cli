68b66edd257e80490347be81929639cf97c86567
fand <mail@amagi.dev>
2024-08-26 11:47:41 -0700

    feat: support STDIN mode

--- README.md
+++ README.md
@@ -36,6 +36,17 @@ All your bases are now under our control.
 
 $ naturalize this command makes the given text into more natural English.
 This command transforms the given text into more natural-sounding English.
+
+
+# It also supports STDIN inputs
+$ echo 'Pipes also work!' | naturalize
+Pipes work too!
+
+$ naturalize
+If no arguments are passed, naturalize will read the text input on the terminal.
+The input will be concatenated until a blank line is inserted, then it shows the output.
+
+If no arguments are passed, 'naturalize' will read the text input from the terminal. The input will continue to be concatenated until a blank line is inserted, at which point the output will be displayed.
 ```
 
 ### `rephrase`
--- bin/naturalize.js
+++ bin/naturalize.js
@@ -1,6 +1,19 @@
 #!/usr/bin/env node
 import { OpenAI } from "openai";
 import rc from "rc";
+import readline from "node:readline";
+import { Writable } from "node:stream";
+
+// https://stackoverflow.com/questions/24037545
+let isStdoutMuted = false;
+const writableStdout = new Writable({
+  write(chunk, encoding, callback) {
+    if (!isStdoutMuted) {
+      process.stdout.write(chunk, encoding);
+    }
+    callback();
+  },
+});
 
 const OPENAI_API_KEY = rc("chatgptenglish").OPENAI_API_KEY;
 if (!OPENAI_API_KEY) {
@@ -11,11 +24,60 @@ if (!OPENAI_API_KEY) {
 const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
 
 (async function main() {
-  const input = process.argv.slice(2).join(" ");
-  await run(input);
+  if (process.argv.length > 2) {
+    const input = process.argv.slice(2).join(" ");
+    await run(input);
+  } else {
+    const isTTY = process.stdin.isTTY;
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: writableStdout,
+      terminal: isTTY,
+    });
+
+    if (isTTY) {
+      rl.setPrompt("> ");
+      rl.prompt();
+    } else {
+      isStdoutMuted = true;
+    }
+
+    let lines = [];
+
+    async function runLines(line_) {
+      const line = line_.trim();
+      if (line === "") {
+        rl.pause();
+        const text = lines.join("\n");
+        lines = [];
+        await run(text);
+        rl.resume();
+      } else {
+        lines.push(line);
+      }
+
+      isTTY && rl.prompt();
+    }
+
+    rl.on("resume", () => {
+      lines = [];
+    });
+    rl.on("line", runLines);
+
+    if (!isTTY) {
+      rl.on("close", () => {
+        isStdoutMuted = false;
+        runLines("");
+      });
+    }
+  }
 })();
 
 async function run(text) {
+  if (text.trim() === "") {
+    return;
+  }
+
   try {
     const messages = generateMessages(text);
     const completion = await openai.chat.completions.create({
